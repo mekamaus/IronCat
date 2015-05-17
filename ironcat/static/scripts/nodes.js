7e60606b47c21002243d42b98b3e08e450f21748
Sam Adams <samadams532@gmail.com>
2015-05-17 01:51:14 -0500

    removed a function that i'm never gonna use

--- ironcat/static/scripts/nodes.js
+++ ironcat/static/scripts/nodes.js
@@ -1444,11 +1444,6 @@
                 this.parentNode.appendChild(this);
             });
         };
-        d3.selection.prototype.sendToBack = function () {
-            return this.each(function () {
-                this.parentNode.prependChild(this);
-            });
-        };
 
         var graph = new GraphCreator(svg, nodes, edges, inputs, outputs);
         graph.setIdCt(2);
