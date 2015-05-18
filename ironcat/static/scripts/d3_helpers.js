fd4e3033454691ca718fe9ce7b9705878a59d42d
Sam Adams <samadams532@gmail.com>
2015-05-17 20:07:11 -0500

    combined d3 helpers into one file

--- ironcat/static/scripts/d3_helpers.js
+++ ironcat/static/scripts/d3_helpers.js
@@ -94,4 +94,9 @@
             });
         });
     };
+    d3.selection.prototype.moveToFront = function () {
+        return this.each(function () {
+            this.parentNode.appendChild(this);
+        });
+    };
 })();
--- ironcat/static/scripts/move_to_front.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    d3.selection.prototype.moveToFront = function () {
-        return this.each(function () {
-            this.parentNode.appendChild(this);
-        });
-    };
-})();
