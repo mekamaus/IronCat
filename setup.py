c6e8f6ab91db561f7ba364fcaf5f4e5fc81bd2b3
Sam Adams <samadams532@gmail.com>
2015-08-06 10:21:01 -0700

    added site-packages folder

--- setup.py
+++
@@ -1,5 +0,0 @@
-pip install --upgrade azure
-pip install --upgrade django
-pip install --upgrade python-dateutil
-pip install --upgrade six
-pip install --upgrade --allow-all-external mysql-connector-python
