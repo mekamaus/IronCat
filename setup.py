d2a9a2752596b95beadc230a696884fa4a5aa3e9
Sam Adams <samadams532@gmail.com>
2015-08-06 10:13:27 -0700

    using setup.py

---
+++ setup.py
@@ -0,0 +1,5 @@
+pip install --upgrade azure
+pip install --upgrade django
+pip install --upgrade python-dateutil
+pip install --upgrade six
+pip install --upgrade --allow-all-external mysql-connector-python
